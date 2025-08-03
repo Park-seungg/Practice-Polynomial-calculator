059fdc97546695ed2d8f7925a176f656eed54053
Park-seungg <parkis1101@gmail.com>
2025-08-03 15:52:12 +0900

    test 8 -blue

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,19 +2,21 @@ package com.ll;
 
 public class Calc {
     public static int run(String expression) {
-        if (expression.equals("10 + 20 + 30")) {
-            return 60;
-        }
-        if (expression.contains(" - ")) {
-            String[] bits = expression.split(" - ");
-            int num1 = Integer.parseInt(bits[0]);
-            int num2 = Integer.parseInt(bits[1]);
-            return num1 - num2;
+        expression = expression.replaceAll(" ", "");
+        if (expression.contains("-")) {
+            String[] bits = expression.split("-");
+            int result = Integer.parseInt(bits[0]);
+            for (int i = 1; i < bits.length; i++) {
+                result -= Integer.parseInt(bits[i]);
+            }
+            return result;
         } else {
-            String[] bits = expression.split(" \\+ ");
-            int num1 = Integer.parseInt(bits[0]);
-            int num2 = Integer.parseInt(bits[1]);
-            return num1 + num2;
+            String[] bits = expression.split("\\+");
+            int result = Integer.parseInt(bits[0]);
+            for (int i = 1; i < bits.length; i++) {
+                result += Integer.parseInt(bits[i]);
+            }
+            return result;
         }
     }
 }
