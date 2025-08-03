359230a339465424f696db41d2fd3bbea85b2eac
Park-seungg <parkis1101@gmail.com>
2025-08-03 15:23:42 +0900

    test 5 -blue

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,15 +2,16 @@ package com.ll;
 
 public class Calc {
     public static int run(String expression) {
-        if (expression.equals("2 - 1")) {
-            return 1;
+        if (expression.contains(" - ")) {
+            String[] bits = expression.split(" - ");
+            int num1 = Integer.parseInt(bits[0]);
+            int num2 = Integer.parseInt(bits[1]);
+            return num1 - num2;
+        } else {
+            String[] bits = expression.split(" \\+ ");
+            int num1 = Integer.parseInt(bits[0]);
+            int num2 = Integer.parseInt(bits[1]);
+            return num1 + num2;
         }
-
-        String[] expressionBits = expression.split(" \\+ ");
-
-        int num1 = Integer.parseInt(expressionBits[0]);
-        int num2 = Integer.parseInt(expressionBits[1]);
-
-        return num1 + num2;
     }
 }
